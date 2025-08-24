7ad26d09fc42c283a331454030ebc5fbaade37f9
ghifari909 <akunzuu2@gmail.com>
2025-08-24 14:13:12 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Uplinx – Upload File Aman</title>
+        <meta name="description" content="Platform upload file terpercaya & aman powered by Next.js + GitHub API." />
+        {/* favicon otomatis kalau ada /public/favicon.ico */}
+        {/* kalau mau custom logo: */}
+        {/* <link rel="icon" href="/logo.png" /> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
