b2e76acfd226ad0b3b6140fac3b9f5337690641f
ghifari909 <akunzuu2@gmail.com>
2025-08-24 14:20:46 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,16 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <title>Uplinx – Upload File Aman</title>
         <meta name="description" content="Platform upload file terpercaya & aman powered by Next.js + GitHub API." />
-        {/* favicon otomatis kalau ada /public/favicon.ico */}
-        {/* kalau mau custom logo: */}
-        {/* <link rel="icon" href="/logo.png" /> */}
+
+        {/* Open Graph untuk WA/Telegram/FB */}
+        <meta property="og:title" content="Uplinx – Upload File Aman" />
+        <meta property="og:description" content="Upload dan share file mudah dengan link langsung." />
+        <meta property="og:image" content="https://upv2.vercel.app/og-thumbnail.png" />
+        <meta property="og:url" content="https://upv2.vercel.app" />
+        <meta property="og:type" content="website" />
+
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
