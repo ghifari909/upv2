04946f73176de1fecad57da12ef384e7fd931413
ghifari909 <akunzuu2@gmail.com>
2025-08-25 20:33:27 +0700

    Update cdn.js

--- pages/api/cdn.js
+++ pages/api/cdn.js
@@ -1,14 +1,19 @@
 export default async function handler(req, res) {
-  const { file, type } = req.query;
+  const { file } = req.query;
   if (!file) return res.status(400).json({ error: "file query wajib" });
 
   const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
-  const publicId = file.split(".")[0];
-  const ext = file.split(".")[1] || "";
 
-  // pake resource_type asli dari Cloudinary
-  const resourceType = type || "raw";
+  // deteksi ekstensi
+  const parts = file.split(".");
+  const publicId = parts.slice(0, -1).join(".");
+  const ext = parts.pop().toLowerCase();
 
-  const url = `https://res.cloudinary.com/${cloudName}/${resourceType}/upload/${publicId}.${ext}`;
+  let type = "raw";
+  if (["jpg","jpeg","png","gif","webp"].includes(ext)) type = "image";
+  if (["mp4","webm","mov","avi","mkv"].includes(ext)) type = "video";
+  if (["mp3","wav","ogg"].includes(ext)) type = "video";
+
+  const url = `https://res.cloudinary.com/${cloudName}/${type}/upload/${publicId}.${ext}`;
   return res.redirect(302, url);
 }
